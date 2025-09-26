c04edcfaa4767a7f2b6abb00c9fe21e902917b63
Polina0013 <p.pozdnova@gmail.com>
2025-09-26 15:24:57 +0300

    Shortened the tests and corrected their names

--- tests/test_circle.cpp
+++ tests/test_circle.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Marina Usova
+// Copyright 2025 Polina Pozdnova
 
 #include <gtest/gtest.h>
 #include "../lib_point/point.h"
@@ -6,117 +6,80 @@
 
 #define EPSILON 0.000001
 
-TEST(TestCircle, test_0_1_the_radius_is_less_than_zero) {
+TEST(TestCircle, the_radius_is_zero) {
     // Arrange
 
     // Act & Assert
     ASSERT_ANY_THROW(Circle<int> z1(3, 4, 0));
 }
 
-TEST(TestCircle, test_0_2_the_radius_is_less_than_zero) {
+TEST(TestCircle, the_radius_is_less_than_zero) {
     // Arrange
 
     // Act & Assert
     ASSERT_ANY_THROW(Circle<int> z2(1, 3, -1));
 }
 
-TEST(TestCircle, test_0_3_the_radius_is_less_than_zero) {
+TEST(TestCircle, the_radius_is_zero_with_class_point) {
     // Arrange
-    Point<int> p1(0, 0), p2(10, -10);
+    Point<int> p(0, 0);
 
     // Act & Assert
-    ASSERT_ANY_THROW(Circle<int> z1(p1, 0));
+    ASSERT_ANY_THROW(Circle<int> z1(p, 0));
 }
 
-TEST(TestCircle, test_0_4_the_radius_is_less_than_zero) {
+TEST(TestCircle, the_radius_is_less_than_zero_with_class_point) {
     // Arrange
-    Point<int> p1(0, 0), p2(10, -10);
+    Point<int> p(10, -10);
 
     // Act & Assert
-    ASSERT_ANY_THROW(Circle<int> z2(p2, -1));
+    ASSERT_ANY_THROW(Circle<int> z2(p, -1));
 }
 
-TEST(TestCircle, test_1_the_circles_match) {
+TEST(TestCircle, the_circles_match) {
     // Arrange
     Circle<float> z1, z2;
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The circles match";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The circles match", z1.check_relative_location(z2));
 }
 
-TEST(TestCircle, test_2_do_not_intersect_or_touch) {
+TEST(TestCircle, do_not_intersect_or_touch) {
     // Arrange
-    Point<int> p1(0, 0), p2(10, -10);
-    Circle<int> z1(p1, 1), z2(p2, 2);
+    Circle<int> z1(Point<int>(0, 0), 1), z2(Point<int> (10, -10), 2);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The circles do not intersect or touch";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The circles do not intersect or touch", z1.check_relative_location(z2));
 }
 
-TEST(TestCircle, test_3_touch_at_one_point_externally) {
+TEST(TestCircle, touch_at_one_point_externally) {
     // Arrange
     Circle<int> z1(5, 5, 5), z2(-5, 5, 5);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The circles touch at one point (externally)";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The circles touch at one point (externally)", z1.check_relative_location(z2));
 }
 
-TEST(TestCircle, test_4_1_intersect_at_two_points) {
+TEST(TestCircle, intersect_at_two_points_and_no_circle_is_inside_the_other) {
     // Arrange
     Circle<float> z1(-2.3f, -3.4f, 5.7f), z2(-1.2f, 1.0f, 2.7f);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The circles intersect at two points";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The circles intersect at two points", z1.check_relative_location(z2));
 }
 
-TEST(TestCircle, test_4_2_intersect_at_two_points) {
+TEST(TestCircle, intersect_at_two_points_and_one_inside_the_other) {
     // Arrange
     Circle<int> z1(10, -2, 5), z2(6, -2, 2);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The circles intersect at two points";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The circles intersect at two points", z1.check_relative_location(z2));
 }
 
-TEST(TestCircle, test_5_1_one_circle_is_inside_the_other) {
+TEST(TestCircle, one_circle_is_inside_the_other) {
     // Arrange
     Circle<double> z1(-2.3, -3.4, 5.7), z2(-2.2, -3.3, 2.5);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "One circle is inside the other";
-    EXPECT_EQ(expected_result, actual_result);
-}
-
-TEST(TestCircle, test_5_2_one_circle_is_inside_the_other) {
-    // Arrange
-    Circle<double> z1(-1.2, 3.4, 1.2), z2(-0.2, 3.4, 0.2);
-
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "One circle is inside the other";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("One circle is inside the other", z1.check_relative_location(z2));
 }
--- tests/test_sphere.cpp
+++ tests/test_sphere.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Marina Usova
+// Copyright 2025 Polina Pozdnova
 
 #include <gtest/gtest.h>
 #include "../lib_point3D/point3D.h"
@@ -21,7 +21,7 @@ TEST(TestSphere, throw_when_try_create_with_init_constructor_by_not_positive_rad
     ASSERT_ANY_THROW(Sphere<int> z1(p1, -1));
 }
 
-TEST(TestSphere, test_1_the_spheres_match) {
+TEST(TestSphere, the_spheres_match) {
     // Arrange
     Sphere<float> z1(10, -10, -10, 5), z2(10, -10, -10, 5);
 
@@ -29,74 +29,50 @@ TEST(TestSphere, test_1_the_spheres_match) {
     EXPECT_EQ("The spheres match", z1.check_relative_location(z2));
 }
 
-TEST(TestSphere, test_2_do_not_intersect_or_touch) {
+TEST(TestSphere, do_not_intersect_or_touch) {
     // Arrange
     Sphere<int> z1(Point3D<int>(0, 0, 0), 1), z2(Point3D<int>(10, -10, -10), 2);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The spheres do not intersect or touch";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The spheres do not intersect or touch", z1.check_relative_location(z2));
 }
 
-TEST(TestSphere, test_3_touch_at_one_point_externally) {
+TEST(TestSphere, touch_at_one_point_externally) {
     // Arrange
     Sphere<int> z1(5, 5, 5, 5), z2(-5, 5, 5, 5);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The spheres touch at one point (externally)";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The spheres touch at one point (externally)", z1.check_relative_location(z2));
 }
 
-TEST(TestSphere, test_4_1_intersect_in_a_circle) {
+TEST(TestSphere, intersect_from_the_outside_in_a_circle) {
     // Arrange
     Sphere<float> z1(-2.3f, -3.4f, 3.0f, 15.7f), z2(-1.2f, 1.0f, -2.2f, 12.7f);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The spheres intersect in a circle";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The spheres intersect in a circle", z1.check_relative_location(z2));
 }
 
-TEST(TestSphere, test_4_2_intersect_in_a_circle) {
+TEST(TestSphere, intersect_from_the_inside_along_a_circle) {
     // Arrange
     Sphere<int> z1(5, 5, 5, 5), z2(6, 6, 6, 5);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "The spheres intersect in a circle";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("The spheres intersect in a circle", z1.check_relative_location(z2));
 }
 
-TEST(TestSphere, test_5_1_smaller_inside_larger) {
+TEST(TestSphere, smaller_inside_larger) {
     // Arrange
     Sphere<int> z1(5, 5, 5, 5), z2(5, 5, 5, 2);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "One sphere is completely inside the other (smaller inside larger)";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("One sphere is completely inside the other (smaller inside larger)", z1.check_relative_location(z2));
 }
 
-TEST(TestSphere, test_5_2_larger_contains_smaller) {
+TEST(TestSphere, larger_contains_smaller) {
     // Arrange
     Sphere<int> z1(5, 5, 5, 2), z2(5, 5, 5, 5);
 
-    // Act
-    std::string actual_result = z1.check_relative_location(z2);
-
-    // Assert
-    std::string expected_result = "One sphere is completely inside the other (larger contains smaller)";
-    EXPECT_EQ(expected_result, actual_result);
+    // Act & Assert
+    EXPECT_EQ("One sphere is completely inside the other (larger contains smaller)", z1.check_relative_location(z2));
 }
