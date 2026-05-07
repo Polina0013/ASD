08893bc03cda3a5fef2cb9a3bbdfd8e086d761c5
Polina0013 <p.pozdnova@gmail.com>
2026-05-07 20:13:27 +0300

    Added some fixes

--- lib_monom/monom.cpp
+++ lib_monom/monom.cpp
@@ -1,3 +1,352 @@
 // Copyright 2026 Pozdnova Polina
 
 #include "..\lib_monom\monom.h"
+
+Monom::Monom() : _coeff(0.0) {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        _powers[i] = 0;
+    }
+}
+Monom::Monom(double coeff) : _coeff(std::abs(coeff) < EPSILON ? 0.0 : coeff), _powers{ 0, 0, 0 } {}
+Monom::Monom(double coeff, const int powers[VARS_COUNTER]) : _coeff(coeff) {
+    if (powers) {
+        for (int i = 0; i < VARS_COUNTER; i++) {
+            if (powers[i] < 0) throw std::logic_error("Monom degree cannot be negative!");
+            _powers[i] = powers[i];
+        }
+    }
+    else {
+        for (int i = 0; i < VARS_COUNTER; i++) {
+            _powers[i] = 0;
+        }
+    }
+}
+Monom::Monom(double coeff, std::initializer_list<int> powers) : _coeff(coeff) {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        _powers[i] = 0;
+    }
+
+    int i = 0;
+    for (int p : powers) {
+        if (i < VARS_COUNTER) {
+            if (p < 0) throw std::logic_error("Monom degree cannot be negative!");
+            _powers[i] = p;
+            i++;
+        }
+    }
+}
+Monom::Monom(std::string monom_str) : _coeff(1.0) {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        _powers[i] = 0;
+    }
+
+    monom_str.erase(remove(monom_str.begin(), monom_str.end(), ' '), monom_str.end());
+
+    if (monom_str.empty() || monom_str == "0") {
+        _coeff = 0.0;
+        return;
+    }
+
+    size_t pos = 0;
+
+    if (monom_str[0] == '+' || monom_str[0] == '-') {
+        if (monom_str[0] == '-') _coeff = -1.0;
+        pos = 1;
+    }
+
+    size_t num_end = pos;
+    while (num_end < monom_str.length() &&
+        (isdigit(monom_str[num_end]) || monom_str[num_end] == '.')) {
+        num_end++;
+    }
+
+    if (num_end > pos) {
+        std::string num_str = monom_str.substr(pos, num_end - pos);
+        double abs_coeff = std::stod(num_str);
+        _coeff = (_coeff < 0) ? -abs_coeff : abs_coeff;
+        pos = num_end;
+    }
+
+    char vars[] = { 'x', 'y', 'z' };
+
+    for (int var_index = 0; var_index < VARS_COUNTER; var_index++) {
+        if (pos < monom_str.length() && monom_str[pos] == vars[var_index]) {
+            pos++;
+
+            int power = 1;
+
+            if (pos < monom_str.length() && monom_str[pos] == '^') {
+                pos++;
+
+                size_t power_start = pos;
+                while (pos < monom_str.length() && isdigit(monom_str[pos])) {
+                    pos++;
+                }
+
+                if (power_start == pos) throw std::logic_error("Expected number after ^ !");
+
+                std::string power_str = monom_str.substr(power_start, pos - power_start);
+                power = std::stoi(power_str);
+
+                if (power < 0) throw std::logic_error("Power cannot be negative!");
+            }
+
+            _powers[var_index] = power;
+        }
+    }
+
+    if (pos < monom_str.length()) throw std::logic_error("Unexpected characters after parsing: " + monom_str.substr(pos));
+
+    if (std::abs(_coeff) < EPSILON) {
+        _coeff = 0.0;
+    }
+}
+Monom::Monom(const Monom& other) : _coeff(other._coeff) {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        _powers[i] = other._powers[i];
+    }
+}
+
+double Monom::get_coeff() const { return _coeff; }
+const int* Monom::get_powers() const { return _powers; }
+int Monom::get_power(int index) const {
+    if (index < 0 || index >= VARS_COUNTER) throw std::out_of_range("Index out of range");
+    return _powers[index];
+}
+
+bool Monom::is_zero() const { return std::abs(_coeff) < EPSILON; }
+
+Monom Monom::add(const Monom& other) const {
+    if (*this != other) throw std::logic_error("Cannot add monomials with different powers!");
+    return Monom(_coeff + other._coeff, _powers);
+}
+Monom Monom::sub(const Monom& other) const {
+    if (*this != other) throw std::logic_error("Cannot add monomials with different powers!");
+    return Monom(_coeff - other._coeff, _powers);
+}
+Monom Monom::mult(const Monom& other) const {
+    Monom res;
+    res._coeff = _coeff * other._coeff;
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        res._powers[i] = _powers[i] + other._powers[i];
+    }
+
+    return res;
+}
+Monom Monom::div(const Monom& other) const {
+    if (std::abs(other._coeff) < EPSILON) throw std::logic_error("Division by zero!");
+
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (_powers[i] < other._powers[i]) throw std::logic_error("Division would result in negative powers!");
+    }
+
+    Monom res;
+    res._coeff = _coeff / other._coeff;
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        res._powers[i] = _powers[i] - other._powers[i];
+    }
+
+    return res;
+}
+
+Monom Monom::mult_by_number(double other) const {
+    return Monom(_coeff * other, _powers);
+}
+Monom Monom::div_by_number(double other) const {
+    if (std::abs(other) < EPSILON) throw std::logic_error("Division by zero!");
+    return Monom(_coeff / other, _powers);
+}
+
+double Monom::value_at_the_point(double x, double y, double z) const {
+    return (_coeff * pow(x, _powers[0]) * pow(y, _powers[1]) * pow(z, _powers[2]));
+}
+
+std::string Monom::to_string() const {
+    if (is_zero()) {
+        return "0";
+    }
+
+    std::string result;
+
+    if (std::abs(_coeff - 1.0) > EPSILON &&
+        std::abs(_coeff + 1.0) > EPSILON) {
+        // коэффициент не +-1
+        result += std::to_string(_coeff);
+        // убираем нули
+        if (result.find('.') != std::string::npos) {
+            result.erase(result.find_last_not_of('0') + 1, std::string::npos);
+            result.erase(result.find_last_not_of('.') + 1, std::string::npos);
+        }
+    }
+    else if (std::abs(_coeff + 1.0) < EPSILON) {
+        // коэффициент равен -1
+        result += "-";
+    }
+
+    bool hasVars = false;
+    char vars[] = { 'x', 'y', 'z' };
+
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (_powers[i] > 0) {
+            hasVars = true;
+            result += vars[i];
+            if (_powers[i] > 1) {
+                result += "^" + std::to_string(_powers[i]);
+            }
+        }
+    }
+
+    // нет переменных, коэффициент равен +-1
+    if (!hasVars) {
+        if (std::abs(_coeff - 1.0) < EPSILON) {
+            result += "1";
+        }
+        else if (std::abs(_coeff + 1.0) < EPSILON) {
+            result += "-1";
+        }
+    }
+
+    return result;
+}
+
+bool Monom::operator==(const Monom& other) const {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (_powers[i] != other._powers[i]) return false;
+    }
+    return true;
+}
+bool Monom::operator!=(const Monom& other) const {
+    return !(*this == other);
+}
+
+Monom& Monom::operator=(const Monom& other) {
+    if (this != &other) {
+        _coeff = other._coeff;
+        for (int i = 0; i < VARS_COUNTER; i++) {
+            _powers[i] = other._powers[i];
+        }
+    }
+    return *this;
+}
+
+Monom Monom::operator+(const Monom& other) const {
+    return this->add(other);
+}
+Monom Monom::operator-(const Monom& other) const {
+    return this->sub(other);
+}
+Monom Monom::operator*(const Monom& other) const {
+    return this->mult(other);
+}
+Monom Monom::operator/(const Monom& other) const {
+    return this->div(other);
+}
+
+Monom Monom::operator*(double other) const {
+    return this->mult_by_number(other);
+}
+Monom Monom::operator/(double other) const {
+    return this->div_by_number(other);
+}
+
+Monom& Monom::operator+=(const Monom& other) {
+    *this = *this + other;
+    return *this;
+}
+Monom& Monom::operator-=(const Monom& other) {
+    *this = *this - other;
+    return *this;
+}
+Monom& Monom::operator*=(const Monom& other) {
+    *this = *this * other;
+    return *this;
+}
+Monom& Monom::operator/=(const Monom& other) {
+    *this = *this / other;
+    return *this;
+}
+
+Monom& Monom::operator*=(double other) {
+    *this = *this * other;
+    return *this;
+}
+Monom& Monom::operator/=(double other) {
+    *this = *this / other;
+    return *this;
+}
+
+Monom Monom::operator-() const {
+    return Monom(-_coeff, _powers);
+}
+
+bool Monom::operator>(const Monom& other) const {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (_powers[i] != other._powers[i]) return _powers[i] > other._powers[i];
+    }
+    return false;
+}
+bool Monom::operator<(const Monom& other) const {
+    return other > *this;
+}
+
+bool Monom::operator<=(const Monom& other) const {
+    return !(other < *this);
+}
+bool Monom::operator>=(const Monom& other) const {
+    return !(other > *this);
+}
+
+std::ostream& operator<<(std::ostream& out, const Monom& monom) {
+    if (monom.is_zero()) {
+        out << "0";
+        return out;
+    }
+
+    if (std::abs(monom._coeff - 1.0) > EPSILON &&
+        std::abs(monom._coeff + 1.0) > EPSILON) {
+        // коэффициент не +-1
+        out << monom._coeff;
+    }
+    else if (std::abs(monom._coeff + 1.0) < EPSILON) {
+        // коэффициент равен -1
+        out << "-";
+    }
+
+    bool hasVars = false;
+    char vars[] = { 'x', 'y', 'z' };
+
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (monom._powers[i] > 0) {
+            hasVars = true;
+            out << vars[i];
+            if (monom._powers[i] > 1) {
+                out << "^" << monom._powers[i];
+            }
+        }
+    }
+
+    // нет переменных, коэффициент равен +-1
+    if (!hasVars) {
+        if (std::abs(monom._coeff - 1.0) < EPSILON) {
+            out << "1";
+        }
+        else if (std::abs(monom._coeff + 1.0) < EPSILON) {
+            out << "-1";
+        }
+    }
+
+    return out;
+}
+std::istream& operator>>(std::istream& in, Monom& monom) {
+    std::string monom_str;
+    in >> monom_str;
+
+    try {
+        monom = Monom(monom_str);
+    }
+    catch (const std::logic_error& e) {
+        in.setstate(std::ios::failbit);
+    }
+
+    return in;
+}
--- lib_polynom/polynom.cpp
+++ lib_polynom/polynom.cpp
@@ -1,3 +1,517 @@
 // Copyright 2026 Pozdnova Polina
 
 #include "..\lib_polynom\polynom.h"
+
+Polynom::Polynom() : _polynom(), _name("") {}
+Polynom::Polynom(const Monom& monom) : _polynom(), _name("") {
+    if (!monom.is_zero()) _polynom.push_back(monom);
+}
+Polynom::Polynom(const Monom& monom, const std::string& name)
+    : _polynom(), _name(name) {
+    if (!monom.is_zero()) _polynom.push_back(monom);
+}
+Polynom::Polynom(const std::string& str) : _polynom(), _name("") {
+    if (str.empty()) return;
+
+    std::string poly_str = str;
+
+    // P = x^2 + y
+    size_t eq_pos = poly_str.find('=');
+    if (eq_pos != std::string::npos) {
+        std::string name_str = poly_str.substr(0, eq_pos);
+        name_str.erase(remove(name_str.begin(), name_str.end(), ' '), name_str.end());
+        if (!name_str.empty()) {
+            _name = name_str;
+        }
+
+        poly_str = poly_str.substr(eq_pos + 1);
+    }
+
+    poly_str.erase(remove(poly_str.begin(), poly_str.end(), ' '), poly_str.end());
+
+    if (poly_str.empty() || poly_str == "0") return;
+
+    size_t pos = 0;
+
+    while (pos < poly_str.length()) {
+        double sign = 1.0;
+        if (poly_str[pos] == '+') {
+            pos++;
+        }
+        else if (poly_str[pos] == '-') {
+            sign = -1.0;
+            pos++;
+        }
+
+        if (pos >= poly_str.length()) {
+            throw std::logic_error("Unexpected end of string after sign!");
+        }
+
+        size_t monom_end = pos;
+        while (monom_end < poly_str.length() &&
+            poly_str[monom_end] != '+' &&
+            poly_str[monom_end] != '-') {
+            monom_end++;
+        }
+
+        std::string monom_str = poly_str.substr(pos, monom_end - pos);
+
+        Monom monom;
+        try {
+            monom = Monom(monom_str);
+        }
+        catch (const std::logic_error& e) {
+            throw std::logic_error("Error parsing monomial '" + monom_str + "': " + e.what());
+        }
+
+        if (sign < 0) {
+            monom = -monom;
+        }
+
+        this->insert_sorted(monom);
+
+        pos = monom_end;
+    }
+}
+Polynom::Polynom(const Polynom& other) : _polynom(other._polynom), _name(other._name) {}
+
+void Polynom::set_name(const std::string& name) { _name = name; }
+const std::string& Polynom::get_name() const {
+    return _name;
+}
+
+bool Polynom::is_zero() const {
+    if (_polynom.is_empty()) return true;
+
+    for (auto it = _polynom.begin(); it != _polynom.end(); it++) {
+        if (!it->is_zero()) return false;
+    }
+
+    return true;
+}
+
+Polynom Polynom::add(const Polynom& other) const {
+    return merge(*this, other);
+}
+Polynom Polynom::sub(const Polynom& other) const {
+    return merge(*this, -other);
+}
+Polynom Polynom::mult(const Polynom& other) const {
+    Polynom result;
+
+    for (auto it1 = _polynom.begin(); it1 != _polynom.end(); ++it1) {
+        for (auto it2 = other._polynom.begin(); it2 != other._polynom.end(); ++it2) {
+            Monom product = (*it1) * (*it2);
+            if (!product.is_zero()) {
+                result.insert_sorted(product);
+            }
+        }
+    }
+
+    return result;
+
+}
+Polynom Polynom::div(const Polynom& other) const {
+    return divmod(other).first;
+}
+Polynom Polynom::mod(const Polynom& other) const {
+    return divmod(other).second;
+}
+std::pair<Polynom, Polynom> Polynom::divmod(const Polynom& other) const {
+    if (other.is_zero()) throw std::logic_error("Division by zero!");
+
+    Polynom quotient;
+    Polynom remainder = *this;
+
+    while (!remainder.is_zero()) {
+        Monom remainder_leading = *remainder._polynom.begin();
+        Monom divisor_leading = *other._polynom.begin();
+
+        if (!can_divide(remainder_leading, divisor_leading)) {
+            break;
+        }
+
+        try {
+            Monom quotient_term = remainder_leading / divisor_leading;
+
+            quotient.insert_sorted(quotient_term);
+
+            Polynom to_sub;
+            for (auto it = other._polynom.begin(); it != other._polynom.end(); it++) {
+                Monom product = (*it) * quotient_term;
+                if (!product.is_zero()) {
+                    to_sub.insert_sorted(product);
+                }
+            }
+            remainder = merge(remainder, -to_sub);
+
+        }
+        catch (const std::logic_error& e) {
+            break;
+        }
+    }
+
+    return { quotient, remainder };
+}
+
+Polynom Polynom::add_a_monom(const Monom& other) const {
+    return *this + Polynom(other);
+}
+Polynom Polynom::sub_a_monom(const Monom& other) const {
+    return *this - Polynom(other);
+}
+Polynom Polynom::mult_by_monom(const Monom& other) const {
+    return *this * Polynom(other);
+}
+Polynom Polynom::div_by_monom(const Monom& other) const {
+    return *this / Polynom(other);
+}
+Polynom Polynom::mod_by_monom(const Monom& other) const {
+    return *this % Polynom(other);
+}
+std::pair<Polynom, Polynom> Polynom::divmod_by_monom(const Monom& other) const {
+    return this->divmod(Polynom(other));
+}
+
+Polynom Polynom::add_a_number(double other) const {
+    return *this + Monom(other);
+}
+Polynom Polynom::sub_a_number(double other) const {
+    return *this - Monom(other);
+}
+Polynom Polynom::mult_by_number(double other) const {
+    return *this * Monom(other);
+}
+Polynom Polynom::div_by_number(double other) const {
+    return *this / Monom(other);
+}
+
+double Polynom::value_at_the_point(double x, double y, double z) const {
+    if (_polynom.is_empty()) return 0.0;
+
+    double res = 0.0;
+    for (auto it = _polynom.begin(); it != _polynom.end(); it++) {
+        res += it->value_at_the_point(x, y, z);
+    }
+
+    return res;
+}
+
+std::string Polynom::to_string() const {
+    if (is_zero()) {
+        return "0";
+    }
+
+    std::string result;
+    auto it = _polynom.begin();
+    result += it->to_string();
+    it++;
+
+    for (; it != _polynom.end(); it++) {
+        const Monom& monom = *it;
+
+        if (monom.get_coeff() < 0) result += " " + monom.to_string();
+        else result += " + " + monom.to_string();
+    }
+
+    return result;
+}
+
+Polynom Polynom::operator-() const {
+    Polynom result;
+    for (auto it = _polynom.begin(); it != _polynom.end(); it++) {
+        result.insert_sorted(-(*it));
+    }
+    result._name = _name;
+    return result;
+}
+Polynom Polynom::operator+(const Polynom& other) const {
+    return this->add(other);
+}
+Polynom Polynom::operator-(const Polynom& other) const {
+    return this->sub(other);
+}
+Polynom Polynom::operator*(const Polynom& other) const {
+    return this->mult(other);
+}
+Polynom Polynom::operator/(const Polynom& other) const {
+    return this->div(other);
+}
+Polynom Polynom::operator%(const Polynom& other) const {
+    return this->mod(other);
+}
+
+Polynom Polynom::operator+(const Monom& other) const {
+    return this->add_a_monom(other);
+}
+Polynom Polynom::operator-(const Monom& other) const {
+    return this->sub_a_monom(other);
+}
+Polynom Polynom::operator*(const Monom& other) const {
+    return this->mult_by_monom(other);
+}
+Polynom Polynom::operator/(const Monom& other) const {
+    return this->div_by_monom(other);
+}
+Polynom Polynom::operator%(const Monom& other) const {
+    return this->mod_by_monom(other);
+}
+
+Polynom operator+(const Monom& monom, const Polynom& polynom) {
+    return Polynom(monom) + polynom;
+}
+Polynom operator-(const Monom& monom, const Polynom& polynom) {
+    return Polynom(monom) - polynom;
+}
+Polynom operator*(const Monom& monom, const Polynom& polynom) {
+    return Polynom(monom) * polynom;
+}
+Polynom operator/(const Monom& monom, const Polynom& polynom) {
+    return Polynom(monom) / polynom;
+}
+Polynom operator%(const Monom& monom, const Polynom& polynom) {
+    return Polynom(monom) % polynom;
+}
+
+Polynom Polynom::operator+(double other) const {
+    return this->add_a_number(other);
+}
+Polynom Polynom::operator-(double other) const {
+    return this->sub_a_number(other);
+}
+Polynom Polynom::operator*(double other) const {
+    return this->mult_by_number(other);
+}
+Polynom Polynom::operator/(double other) const {
+    return this->div_by_number(other);
+}
+
+Polynom operator+(double number, const Polynom& polynom) {
+    return Polynom(number) + polynom;
+}
+Polynom operator-(double number, const Polynom& polynom) {
+    return Polynom(number) - polynom;
+}
+Polynom operator*(double number, const Polynom& polynom) {
+    return Polynom(number) * polynom;
+}
+Polynom operator/(double number, const Polynom& polynom) {
+    return Polynom(number) / polynom;
+}
+
+Polynom& Polynom::operator+=(const Polynom& other) {
+    *this = merge(*this, other);
+    return *this;
+}
+Polynom& Polynom::operator-=(const Polynom& other) {
+    *this = merge(*this, -other);
+    return *this;
+}
+Polynom& Polynom::operator*=(const Polynom& other) {
+    *this = *this * other;
+    return *this;
+}
+Polynom& Polynom::operator/=(const Polynom& other) {
+    *this = *this / other;
+    return *this;
+}
+
+Polynom& Polynom::operator+=(const Monom& other) {
+    this->insert_sorted(other);
+    return *this;
+}
+Polynom& Polynom::operator-=(const Monom& other) {
+    this->insert_sorted(-other);
+    return *this;
+}
+Polynom& Polynom::operator*=(const Monom& other) {
+    *this = *this * other;
+    return *this;
+}
+Polynom& Polynom::operator/=(const Monom& other) {
+    *this = *this / other;
+    return *this;
+}
+
+Polynom& Polynom::operator+=(double other) {
+    *this = *this + other;
+    return *this;
+}
+Polynom& Polynom::operator-=(double other) {
+    *this = *this - other;
+    return *this;
+}
+Polynom& Polynom::operator*=(double other) {
+    *this = *this * other;
+    return *this;
+}
+Polynom& Polynom::operator/=(double other) {
+    *this = *this / other;
+    return *this;
+}
+
+bool Polynom::operator==(const Polynom& other) const {
+    auto it1 = _polynom.begin();
+    auto it2 = other._polynom.begin();
+
+    for (; it1 != _polynom.end() && it2 != other._polynom.end(); it1++, it2++) {
+        if (*it1 != *it2) return false;
+    }
+
+    return it1 == _polynom.end() && it2 == other._polynom.end();
+}
+bool Polynom::operator!=(const Polynom& other) const {
+    return !(*this == other);
+}
+
+Polynom& Polynom::operator=(const Polynom& other) {
+    if (this != &other) {
+        _polynom = other._polynom;
+        _name = other._name;
+    }
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const Polynom& polynom) {
+    if (polynom.is_zero()) {
+        out << "0";
+        return out;
+    }
+
+    auto it = polynom._polynom.begin();
+    out << *it;
+    it++;
+
+    for (; it != polynom._polynom.end(); it++) {
+        const Monom& monom = *it;
+
+        if (monom.get_coeff() < 0) out << " " << monom;
+        else out << " + " << monom;
+    }
+
+    return out;
+}
+std::istream& operator>>(std::istream& in, Polynom& polynom) {
+    std::string poly_str;
+    std::getline(in, poly_str);
+
+    try {
+        polynom = Polynom(poly_str);
+    }
+    catch (const std::logic_error& e) {
+        in.setstate(std::ios::failbit);
+    }
+
+    return in;
+}
+
+bool Polynom::check_powers(const Polynom& other) const {
+    if (_polynom.is_empty()) return true;
+    if (other._polynom.is_empty()) return false;
+
+    int max_dividend[VARS_COUNTER] = { 0 };
+    for (auto it = _polynom.begin(); it != _polynom.end(); it++) {
+        for (int i = 0; i < VARS_COUNTER; i++) {
+            int power = it->get_power(i);
+            if (power > max_dividend[i]) max_dividend[i] = power;
+        }
+    }
+
+    int max_divisor[VARS_COUNTER] = { 0 };
+    for (auto it = other._polynom.begin(); it != other._polynom.end(); it++) {
+        for (int i = 0; i < VARS_COUNTER; i++) {
+            int power = it->get_power(i);
+            if (power > max_divisor[i]) max_divisor[i] = power;
+        }
+    }
+
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (max_divisor[i] > max_dividend[i]) return false;
+    }
+
+    return true;
+}
+bool Polynom::can_divide(const Monom& dividend, const Monom& divisor) const {
+    for (int i = 0; i < VARS_COUNTER; i++) {
+        if (dividend.get_power(i) < divisor.get_power(i)) {
+            return false;
+        }
+    }
+    return true;
+}
+void Polynom::insert_sorted(const Monom& monom) {
+    if (monom.is_zero()) return;
+
+    if (_polynom.is_empty()) {
+        _polynom.push_back(monom);
+        return;
+    }
+
+    auto it = _polynom.begin();
+    auto prev_it = _polynom.end();
+
+    while (it != _polynom.end()) {
+        if (*it == monom) {
+            Monom sum = *it + monom;
+            if (sum.is_zero()) {
+                auto node_to_delete = it.get_node();
+                it++;
+                _polynom.erase(node_to_delete);
+            }
+            else {
+                *it = sum;
+            }
+            return;
+        }
+        else if (monom > *it) {
+            if (prev_it == _polynom.end()) {
+                _polynom.push_front(monom);
+            }
+            else {
+                _polynom.insert(prev_it.get_node(), monom);
+            }
+            return;
+        }
+        prev_it = it;
+        it++;
+    }
+
+    _polynom.push_back(monom);
+}
+Polynom Polynom::merge(const Polynom& a, const Polynom& b) const {
+    Polynom result;
+
+    auto it1 = a._polynom.begin();
+    auto it2 = b._polynom.begin();
+
+    while (it1 != a._polynom.end() && it2 != b._polynom.end()) {
+        if (*it1 == *it2) {
+            Monom sum = *it1 + *it2;
+            if (!sum.is_zero()) {
+                result._polynom.push_back(sum);
+            }
+            it1++;
+            it2++;
+        }
+        else if (*it1 > *it2) {
+            result._polynom.push_back(*it1);
+            it1++;
+        }
+        else {
+            result._polynom.push_back(*it2);
+            it2++;
+        }
+    }
+
+    while (it1 != a._polynom.end()) {
+        result._polynom.push_back(*it1);
+        ++it1;
+    }
+
+    while (it2 != b._polynom.end()) {
+        result._polynom.push_back(*it2);
+        ++it2;
+    }
+
+    return result;
+}
